aa92bf5e2f74663039483c80ee664ef217ce7bad
Arqum-Ali <140241880+Arqum-Ali@users.noreply.github.com>
2024-10-09 22:14:49 +0500

    Your commit message here

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Login from "./pages/Login";
+import FacebookLogin from "./pages/FacebookLogin";
+import ShopifyLogin from "./pages/ShopifyLogin";
+
+import FacebookCallback from "./pages/FacebookCallback";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="h-full w-full">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/callback" element={<FacebookCallback />} />
+
+          <Route path="/facebook-login" element={<FacebookLogin />} />
+          <Route path="/shopify-login" element={<ShopifyLogin />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
